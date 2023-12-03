df57338197c87133c19908fe73b59eb96a659309
Kenneth <kennethkarlsson81@protonmail.com>
2023-12-03 13:09:46 +0100

    Format code with Black

--- patterns/strategy_classes.py
+++ patterns/strategy_classes.py
@@ -1,18 +1,22 @@
 from typing import List
 
+
 class StringManipulatorStrategy:
     def mainipulate_string(self, data: List[int]) -> List[int]:
         raise NotImplementedError
+
 
 class Reverse(StringManipulatorStrategy):
     def manipulate_string(self, string: str) -> str:
         # Implement reverse
         return "".join(reversed(string))  # Simplified for demonstration
 
+
 class Capitals(StringManipulatorStrategy):
     def manipulate_string(self, string: str) -> str:
         # Implement upper here
         return string.upper()  # Simplified for demonstration
+
 
 class ManipulatedString:
     def __init__(self, strategy: StringManipulatorStrategy):
@@ -20,6 +24,7 @@ class ManipulatedString:
 
     def manipulate_string(self, string: str) -> str:
         return self._strategy.manipulate_string(string)
+
 
 if __name__ == "__main__":
     # Small dataset
--- patterns/strategy_functional.py
+++ patterns/strategy_functional.py
@@ -1,14 +1,15 @@
 def reverse(string: str) -> str:
     return "".join(reversed(string))
 
+
 def capital_letters(string: str) -> str:
     return string.upper()
+
 
 def string_manipulator(func: callable, string: str) -> str:
     return func(string)
 
+
 if __name__ == "__main__":
     print(string_manipulator(capital_letters, "Hello World!"))
     print(string_manipulator(reverse, "Hello World!"))
-    
-    
